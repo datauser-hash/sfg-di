f31e8a7af880eccc4b6b864e3ad221b8bab76de1
datauser-hash <hovhannes.programmer88@gmail.com>
2022-01-22 19:15:47 +0400

    [Qualifiers explanation]

---
+++ src/main/java/guru/springframework/sfgdi/controllers/SetterInjectedController.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class SetterInjectedController {
+
+    private GreetingService greetingService;
+
+    public String getGreeting() {
+        return this.greetingService.sayGreeting();
+    }
+
+    @Qualifier("setterInjectedGreetingServiceImpl")
+    @Autowired
+    public void setGreetingService(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+}
