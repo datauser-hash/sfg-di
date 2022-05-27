769f1b5b9afe83096d6b5efee77bfa84224b2f2c
datauser-hash <java.datauser@gmail.com>
2022-05-27 07:46:17 +0400

    [Component scan]

--- src/main/java/guru/springframework/pets/CatPetServiceImpl.java
+++ src/main/java/guru/springframework/pets/CatPetServiceImpl.java
@@ -1,6 +1,5 @@
-package guru.springframework.sfgdi.services.impl;
+package guru.springframework.pets;
 
-import guru.springframework.sfgdi.services.PetService;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
